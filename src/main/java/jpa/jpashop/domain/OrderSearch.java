92686d0742e843ca168d98a2687c68afec83bb06
HiPark <youuy28@gmail.com>
2022-09-04 20:18:18 +0900

    Add Order Search Logic

---
+++ src/main/java/jpa/jpashop/domain/OrderSearch.java
@@ -0,0 +1,12 @@
+package jpa.jpashop.domain;
+
+import jpa.jpashop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderSearch {
+
+    private String memberName;
+    private OrderStatus orderStatus;
+}
